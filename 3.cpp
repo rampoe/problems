553728904d74eeec438bc79c14bf1598c46e48a9
rampoe <mail.rampoe@gmail.com>
2024-03-11 15:32:30 +0500

    sync

--- 3.cpp
+++ 3.cpp
@@ -1,16 +1,21 @@
 // В заданной строке заменить каждую цифру символом "*".
 
+#include <cstdlib>
 #include <iostream>
 #include <string>
+#include <cctype>
 
 int main()
 {
     std::string input_string = "";
     std::getline(std::cin, input_string);
-    std::string output_string = ""
-    for (int i = 0; i < input_string.length(); i++)
+    std::string output_string = "";
+    for (unsigned long long int i = 0; i < input_string.length(); i++)
     {
-
+        if (std::isdigit(input_string[i])) output_string += "*";
+        else output_string += input_string[i];
     }
+    std::cout << "Output: " << output_string << std::endl;
+    std::system("pause");
     return 0;
 }
