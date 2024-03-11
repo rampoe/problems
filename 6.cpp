00b2e8dd8e42380fdec36bc59b7dd290fee8f244
rampoe <mail.rampoe@gmail.com>
2024-03-11 16:14:03 +0500

    sync

---
+++ 6.cpp
@@ -0,0 +1,13 @@
+// Построить первые N строк треугольника Паскаля.
+
+#include <cstdlib>
+#include <iostream>
+
+int main()
+{
+    unsigned long long int n = 0;
+    std::cout << "N = ";
+    std::cin >> n;
+    std::system("pause");
+    return 0;
+}
