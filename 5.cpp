38441dfce6c34fbe1cac4ce05e0df317363ceb5f
rampoe <mail.rampoe@gmail.com>
2024-03-11 15:53:22 +0500

    sync

--- 5.cpp
+++ 5.cpp
@@ -1,2 +1,32 @@
 // В заданной строке удалить все пробелы. Если пробелов не было,
 // выдать сообщение об этом, иначе напечатать количество удалённых пробелов.
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main()
+{
+	std::string input_string = "";
+	std::getline(std::cin, input_string);
+	unsigned long long int counter = 0;
+	for (unsigned long long int i = 0; i < input_string.length(); i++)
+	{
+		if (std::isspace(input_string[i]))
+		{
+			counter++;
+		}
+	}
+	if (counter == 0)
+	{
+		std::cout << "No spaces were found.";
+	}
+	else
+	{
+		std::cout << counter << " spaces were deleted from the string.";
+	}
+	std::cout << std::endl;
+	std::system("pause");
+	return 0;
+}
