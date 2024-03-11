2c242af6ac29a9121bc5c24876d0ac2283da33ac
rampoe <mail.rampoe@gmail.com>
2024-03-11 15:29:48 +0500

    sync

--- 2.cpp
+++ 2.cpp
@@ -1,3 +1,33 @@
 // В заданной строке посчитать количество слов. Разделителем слов
 // считается один или несколько пробелов. (Учесть возможность
 // наличия пробелов в начале или конце строки, которые не разделяют слова).
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s, ns;
+    int i = 0, c = 1;
+    getline(cin, s);
+    int j = s.length() - 1;
+    while (isspace(s[i]))
+        i++;
+    while (isspace(s[j]))
+        j--;
+    char sb = '\0';
+    for (int k = i; k <= j; k++)
+    {
+        char cs = s[k];
+        if (!(isspace(cs) && isspace(sb)))
+            ns += cs;
+        sb = cs;
+    }
+    for (int k = 0; k < ns.length(); k++)
+        if (ns[k] == ' ')
+            c++;
+    cout << c;
+    return 0;
+}
--- 3.cpp
+++ 3.cpp
@@ -1,1 +1,16 @@
 // В заданной строке заменить каждую цифру символом "*".
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string input_string = "";
+    std::getline(std::cin, input_string);
+    std::string output_string = ""
+    for (int i = 0; i < input_string.length(); i++)
+    {
+
+    }
+    return 0;
+}
