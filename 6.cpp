be044312f274b00f42ac882c8ae6a781f384bf56
rampoe <mail.rampoe@gmail.com>
2024-03-11 17:40:43 +0500

    sync

--- 6.cpp
+++ 6.cpp
@@ -8,32 +8,32 @@ int main()
     unsigned long long int n = 0;
     std::cout << "N = ";
     std::cin >> n;
-    unsigned long long int triangle[100][100];
+    unsigned long long int triangle[n][n];
     for (unsigned long long int i = 0; i < n; i++)
     {
-    	for (unsigned long long int j = 0; j <= i; j++)
-    	{
-    		triangle[i][j] = 1;
-		}
-	}
-	for (unsigned long long int i = 0; i < n; i++)
+        for (unsigned long long int j = 0; j <= i; j++)
+        {
+            triangle[i][j] = 1;
+        }
+    }
+    for (unsigned long long int i = 0; i < n; i++)
     {
-    	for (unsigned long long int j = 0; j <= i; j++)
-    	{
-    		if (j != 0 && j != i)
-    		{
-    			triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
-			}
-		}
-	}
-	for (unsigned long long int i = 0; i < n; i++)
+        for (unsigned long long int j = 0; j <= i; j++)
+        {
+            if (j != 0 && j != i)
+            {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
+    }
+    for (unsigned long long int i = 0; i < n; i++)
     {
-    	for (unsigned long long int j = 0; j <= i; j++)
-    	{
-    		std::cout << triangle[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
+        for (unsigned long long int j = 0; j <= i; j++)
+        {
+            std::cout << triangle[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
     std::system("pause");
     return 0;
 }
